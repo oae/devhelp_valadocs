81b61440c2ab1874d58f3c32376ec99d19d03900
Osman Alperen Elhan <alperen@elhan.org>
2013-06-07 21:26:30 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+mkdir .books
+wget -r -l2 -nd -Nc -A.tar.bz2 -P "./books/" http://valadoc.org/
+mkdir -p ~/.local/share/devhelp > /dev/null 2>&1
+find "$PWD/books" -iname "*.bz2" -exec tar xf {} -C ".books/" \;
+ln -sf "$PWD/.books" ~/.local/share/devhelp/books
---
+++ update_books.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+wget -r -l2 -nd -Nc -A.tar.bz2 -P "./books/" http://valadoc.org/
