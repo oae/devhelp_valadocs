64b570c7738b57e3c4b6652bcb15be3115e56b19
Osman Alperen Elhan <alperen@elhan.org>
2013-08-07 05:06:01 +0300

    fix install and update issues

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-mkdir .books
+mkdir .books books
 wget -r -l2 -nd -Nc -A.tar.bz2 -P "./books/" http://valadoc.org/
 mkdir -p ~/.local/share/devhelp > /dev/null 2>&1
 find "$PWD/books" -iname "*.bz2" -exec tar xf {} -C ".books/" \;
--- update_books.sh
+++ update_books.sh
@@ -1,2 +1,3 @@
 #/bin/bash
 wget -r -l2 -nd -Nc -A.tar.bz2 -P "./books/" http://valadoc.org/
+find "$PWD/books" -iname "*.bz2" -exec tar xf {} -C ".books/" \;
